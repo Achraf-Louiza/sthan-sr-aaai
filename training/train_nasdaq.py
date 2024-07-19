bd9b645da9ff165203b75bb49a9d0efd0d516d62
Achraf LOUIZA <achraf.louiza1@starteckinnov.com>
2024-07-19 16:46:57 +0200

    model to cuda

--- training/train_nasdaq.py
+++ training/train_nasdaq.py
@@ -43,7 +43,7 @@ def weighted_mse_loss(input, target, weight):
 def trr_loss_mse_rank(pred, base_price, ground_truth, mask, alpha, no_stocks):
     return_ratio = torch.div((pred - base_price), base_price)
     reg_loss = weighted_mse_loss(return_ratio, ground_truth, mask)
-    all_ones = torch.ones(no_stocks, 1, device=device)
+    all_ones = torch.ones(no_stocks, 1)
     pre_pw_dif = (torch.matmul(return_ratio, torch.transpose(all_ones, 0, 1)) 
                   - torch.matmul(all_ones, torch.transpose(return_ratio, 0, 1)))
     gt_pw_dif = (torch.matmul(all_ones, torch.transpose(ground_truth, 0, 1)) -
